f9aa2d89c945f39172830ec385f193e8404a8f46
Karthick <58014955+karthi12503@users.noreply.github.com>
2021-10-13 10:30:43 +0530

    automorphic

--- autotrphic.py
+++ autotrphic.py
@@ -4,6 +4,6 @@ square = str(square)
 last = square[-1]
 last = int(last)
 if last == number:
-    print(number,"is a autotrophic number")
+    print(number,"is a automorphic number")
 else:
-    print(number,"is not a autotrophic number")
+    print(number,"is not a automorphic number")
